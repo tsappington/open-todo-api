14b09a37cf2eb70c998f271a4b7d4f01ec6039b4
Shannon Bertucci <hello@shannon.io>
2017-07-24 21:01:44 -0600

    Renames back to app

--- public/apps/src/scripts/controllers/HomeCtrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    function HomeCtrl(Fixtures) {
-			this.tasks = Fixtures.getTasks();
-    }
-
-    angular
-        .module('blocitoff')
-        .controller('HomeCtrl', ['Fixtures', HomeCtrl]);
-})();
