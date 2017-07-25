7940eb4aa6a981c55a86e42a960cf03942eedebf
Shannon Bertucci <hello@shannon.io>
2017-07-24 21:27:50 -0600

    Haz a login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
   before_save { self.email = email.downcase if email.present? }
 
   validates :email, length: { minimum: 4, maximum: 100 }, presence: true
 
   has_many :lists, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
-  # resources :users, only: [:new, :create]
   namespace :api, defaults: { format: :json } do
 		mount_devise_token_auth_for 'User', at: 'auth'
-		# resources :auth
 
     resources :users do
       resources :lists
